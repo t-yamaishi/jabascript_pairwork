fedcc7b9a3b660c2e132ec76c96290a050ab8f83
yamaishi <yamakios8@icloud.com>
2022-09-12 17:42:31 +0900

    jsの変更

--- pairwork_javascript/scoring.js
+++ pairwork_javascript/scoring.js
@@ -1,21 +1,31 @@
-function get_achievement(points){
-  let sum = points[0];
-    sum = sum + points[1];
-    sum = sum + points[2];
-    sum = sum + points[3];
-    sum = sum + points[4];
 
-  if(sum>=400){
+function get_achievement(points) {
+  let sum = points[0];
+  sum = sum + points[1];
+  sum = sum + points[2];
+  sum = sum + points[3];
+  sum = sum + points[4];
+
+  $("#sum_indicate").text(sum);
+
+  if (sum >= 400) {
     return "A"
   }
-    else if(sum>=300){
+  else if (sum >= 300) {
     return "B";
-  } else if(sum>=200){
+  } else if (sum >= 200) {
     return "C";
-  } else if(sum>=100){
+  } else if (sum >= 100) {
     return "D";
-  }  return "E"
+  } else {
+    return "E"
+  }
 }
 
-let points=[0,0,0,0,0]
-console.log(get_achievement(points))
+let points = [0, 0, 0, 0, 0]
+
+console.log(get_achievement(points));
+
+$('#btn-evaluation').click(function () {
+  $("#evaluation").text(get_achievement(points));
+});
